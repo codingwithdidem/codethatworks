8b6dc673b6462ea775e2c00d05b635de37ed63c2
Ashley20 <kucukkaraaslan.didem@gmail.com>
2021-03-16 15:39:19 +0300

    fix mdx purge issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,33 @@ const { spacing, colors, fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
+    mode: 'all',
     content: [
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
       './layouts/**/*.{js,ts,jsx,tsx}',
-      './data/**/*.{mdx}'
+      './data/**/*.{js,mdx}'
     ],
     options: {
-      safelist: ['prose']
+      safelist: ['prose'],
+      extractors: [
+        {
+          extensions: ['mdx'],
+          extractor: (content) => {
+            content = mdx.sync(content);
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches =
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) ||
+              [];
+
+            return broadMatches.concat(innerMatches);
+          }
+        }
+      ]
     }
   },
   darkMode: 'class', // or 'media' or 'class'
