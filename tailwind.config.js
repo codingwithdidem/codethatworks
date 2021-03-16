713720387ccafd082cde0f40415014c93242d37a
Ashley20 <kucukkaraaslan.didem@gmail.com>
2021-03-16 14:54:07 +0300

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 const { spacing, colors, fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    './data/blog/*.{mdx}'
-  ],
+  purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
