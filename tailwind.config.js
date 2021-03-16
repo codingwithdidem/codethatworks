b42725bfd2d1f18a964bf9e9f86402158c93a807
Ashley20 <kucukkaraaslan.didem@gmail.com>
2021-03-16 15:53:30 +0300

    h

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const { spacing, colors, fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
-    mode: 'all',
     content: [
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
